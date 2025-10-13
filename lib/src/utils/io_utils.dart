3d18cf5ba0fbcac2ef19335981c86a8d0a278ed8
IgnacioFernandez1311 <chonp64@gmail.com>
2025-10-13 01:12:04 -0300

    Change the template provider from Directory to Isolate.resolvePackageUri

--- lib/src/utils/io_utils.dart
+++ lib/src/utils/io_utils.dart
@@ -1,20 +1,29 @@
 import 'dart:io';
+import 'dart:isolate';
 import 'package:path/path.dart' as p;
 
 Future<void> copyTemplate(String template, Directory destination) async {
-  final Directory templateDir = Directory(
-    p.join('lib', 'src', 'templates', '${template}_template'),
+  final uri = await Isolate.resolvePackageUri(
+    Uri.parse('package:pulsar_cli/src/templates/$template'),
   );
 
-  if (!templateDir.existsSync()) {
+  if (uri == null) {
+    throw Exception('Template $template not found');
+  }
+
+  final Directory templateDir = Directory.fromUri(uri);
+
+  if (!await templateDir.exists()) {
     throw Exception('Template "$template" not found');
   }
   await for (final entity in templateDir.list(recursive: true)) {
     if (entity is File) {
       final relativePath = p.relative(entity.path, from: templateDir.path);
+      final newPath = p.join(destination.path, relativePath);
       final newFile = File(p.join(destination.path, relativePath));
       await newFile.create(recursive: true);
-      await newFile.writeAsBytes(await entity.readAsBytes());
+      await entity.copy(newPath);
     }
   }
+  stdout.writeln('Project created from template $template.');
 }
